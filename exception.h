b8a909035ea57fa7da96011d6e56a6b1272b53d4
Julien Rebetez <julien.rebetez@refast-swiss.com>
2016-02-11 00:14:50 +0100

    Exception function & CPU speed optimization

---
+++ exception.h
@@ -0,0 +1,23 @@
+/********************************************************************************************************************/
+/*  Name of the file:       exception.h                                                              
+/*  Purpose of the file:    Headers of all functions related to ...                                         
+/*  Creator:                julien_rebetez                                                                      
+/*  Date of creation:       February 10, 2016                                                                       
+/*                                                                                                                  
+/*  Last modification on:   -                                                                                       
+/*  Modified by:            -                                                                                       
+/*  Version:                -                                                                                       
+/*                                                                                                                  
+/*  List of functions:      -                                                                                       
+/* ******************************************************************************************************************/
+
+#ifndef EXCEPTION_H
+#define	EXCEPTION_H
+
+
+// Exception ExcCode Macros (see Table 50-12 in FRM Section-50 CPU - DS61192)
+
+#define EXCCODE_ADDRESS_ERROR_STORE 5
+#define EXCCODE_TRAP_EXCEPTION 13
+
+#endif	/* EXCEPTION_H */
